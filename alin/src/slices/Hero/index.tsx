bfebe7d6a51c5095781621b7204fa3effc0e201a
Costache Florian-Alin <91316565+Fluffkin23@users.noreply.github.com>
2024-05-28 20:44:40 +0200

    Letter animation done

--- alin/src/slices/Hero/index.tsx
+++ alin/src/slices/Hero/index.tsx
@@ -1,5 +1,10 @@
+'use client';
 import { Content, KeyTextField } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
+import gsap from "gsap";
+import { useEffect,useRef } from "react";
+
+
 
 /**
  * Props for `Hero`.
@@ -10,6 +15,54 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  * Component for "Hero" Slices.
  */
 const Hero = ({ slice }: HeroProps): JSX.Element => {
+
+  const component = useRef(null)
+
+//CHECK DOCUMENTATION ON GSAP MORE
+  useEffect(() => {
+    let ctx = gsap.context(() =>
+    {
+      const tl = gsap.timeline()
+        
+      tl.fromTo(".name-animation", {
+        x:-100, 
+        opacity:0, 
+        rotate: -10
+      },
+      {
+        x: 0, 
+        opacity: 1, 
+        rotate:0,
+        ease: "bounce.out",
+        duration: 2.5,
+        delay: 0.5,
+        transformOrigin: "left top",
+        stagger:
+        {
+          each: 0.1,
+          from: "random",
+        },
+      }
+    );
+
+    tl.fromTo(".job-title", {
+      y: 20,
+      opacity: 0,
+      scale: 1.2
+
+    },
+    {
+      opacity: 1,
+      y: 0,
+      duration: 2,
+      scale: 1,
+      ease: "elastic.out(1,0.3)",
+    });
+
+    },component)
+    return() => ctx.revert();
+  }, []);
+
 
 const renderLetters = (name:KeyTextField, key:string) =>{
   if(!name) return;
@@ -25,6 +78,7 @@ const renderLetters = (name:KeyTextField, key:string) =>{
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      ref = {component}
     >
       <div className="grid min-h-[70vh] grid-cols-1 md:grid-cols-2 items-center">
         <div className="col-start-1 md:row-star-1">
@@ -39,8 +93,8 @@ const renderLetters = (name:KeyTextField, key:string) =>{
               {renderLetters(slice.primary.last_name,"last" )}
             </span>
           </h1>
-            <span className="block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-2xl
-            font-bold uppercase tracking-[.2em] text-transparent opacity-100 md:text-4xl"> {slice.primary.tag_line} </span>
+            <span className=" job-title block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-2xl
+            font-bold uppercase tracking-[.2em] text-transparent opacity-0 md:text-4xl"> {slice.primary.tag_line} </span>
 
         </div>
       </div>
